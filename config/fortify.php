cbbd1ab9c652d926cfb508da9d68816889110b59
Dave Kimble <dave@thedirtdestroyer.com>
2026-04-30 12:54:06 -0400

    Disable public registration and remove register UI links

--- config/fortify.php
+++ config/fortify.php
@@ -144,7 +144,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        // Features::registration(),
         Features::resetPasswords(),
         /* ===Enable these features as needed===
         Features::emailVerification(),
